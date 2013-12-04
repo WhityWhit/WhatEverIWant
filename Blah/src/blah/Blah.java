3d51b8f29da1f1b85286bb1aac6bfd22d25773c0
WhityWhit <jhwhitner5@hotmail.com>
2013-12-04 11:46:59 -0500

    added system.out.println

--- Blah/src/blah/Blah.java
+++ Blah/src/blah/Blah.java
@@ -14,6 +14,6 @@ public class Blah {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic her
+       System.out.println("Hi");
     }
 }
